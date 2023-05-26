a560deaa2ba6ae820f2f47bf1dc4cf0d6094e67b
James Rampton <me@jamesrampton.com>
2023-05-26 13:24:07 +0100

    Try out some other ways to write to a csv

--- examples/write.rs
+++ examples/write.rs
@@ -5,8 +5,14 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     wtr.write_record(&["City", "State", "Population", "Latitude", "Longitude"])?;
     wtr.write_record(&["Davidsons Landing", "AK", "", "65.2419444", "-165.2716667"])?;
-    wtr.write_record(&["Kenai", "AK", "7610", "60.5544444", "-151.2583333"])?;
-    wtr.write_record(&["Oakman", "AL", "", "33.7133333", "-87.3886111"])?;
+    wtr.write_record(vec!["Kenai", "AK", "7610", "60.5544444", "-151.2583333"])?;
+    wtr.write_record(&csv::StringRecord::from(vec![
+        "Oakman",
+        "AL",
+        "",
+        "33.7133333",
+        "-87.3886111",
+    ]))?;
     wtr.flush()?;
 
     Ok(())
