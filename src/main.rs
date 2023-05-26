960c9f581cc5f6517a307f9862520ce6d25ed60d
James Rampton <me@jamesrampton.com>
2023-05-26 08:19:48 +0100

    Handle errors gracefully (part 2)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use std::{io, process};
+use std::{error::Error, io, process};
 
 fn main() {
+    if let Err(err) = run() {
+        println!("error reading csv from <stdin>: {}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
         match result {
+            Err(err) => return Err(From::from(err)),
             Ok(record) => println!("{:?}", record),
-            Err(err) => {
-                println!("error reading csv from <stdin>: {}", err);
-                process::exit(1);
-            }
         }
     }
+    Ok(())
 }
