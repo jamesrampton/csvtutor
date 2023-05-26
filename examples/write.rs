2a8b28ed59924bd50b2db16c88b5ecb73f9b6a90
James Rampton <me@jamesrampton.com>
2023-05-26 13:15:29 +0100

    Implement simple writer for stdout

---
+++ examples/write.rs
@@ -0,0 +1,20 @@
+use std::{error::Error, process};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut wtr = csv::Writer::from_writer(std::io::stdout());
+
+    wtr.write_record(&["City", "State", "Population", "Latitude", "Longitude"])?;
+    wtr.write_record(&["Davidsons Landing", "AK", "", "65.2419444", "-165.2716667"])?;
+    wtr.write_record(&["Kenai", "AK", "7610", "60.5544444", "-151.2583333"])?;
+    wtr.write_record(&["Oakman", "AL", "", "33.7133333", "-87.3886111"])?;
+    wtr.flush()?;
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("error reading csv from <stdin>: {}", err);
+        process::exit(1);
+    }
+}
